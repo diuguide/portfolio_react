7401a324d1af901d6abf8f2256b397b5f5b14634
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-04 11:41:13 -0400

    set up basic routing, test page works

---
+++ porfolio_react/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Home extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+               <h1>test</h1> 
+            </div>
+        )
+    }
+}
+
+export default Home

26358729ed093de58648917c6e33ea5022c30054
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-05 15:49:41 -0400

    Home page better

--- porfolio_react/src/components/About/index.js
+++ porfolio_react/src/components/About/index.js
@@ -3,15 +3,26 @@ import React from 'react';
 function About(props) {
     return (
         <div className="row justify-content-center">
-            <div class="col bg-light rounded p-2 shadow">
-                I am a Web developer with a background in hospitality and communication. I have a proven ability to focus on the
+            <div class="col">
+                
+                <div className="row">
+                    <div className="col m-1 p-3 rounded shadow bg-light">
+                        I am a Web developer with a background in hospitality and communication. I have a proven ability to focus on the
                 customer experience while enhancing a brand message. I excel in fast paced, team oriented
                 environments that require hard work, attention to detail, and the ability to adapt. My objective
                 is to join an entrepreneurial web development team with a focus on mobile-first design and
-                building a better web-experience.
+                building a better web-experience. 
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col m-1 p-3 rounded shadow bg-light">
+                        <p>     Hello there!
+                       Thank you for taking the time to visit my portfolio, here you will find links to all of my work in addition to links to my social media and professional accounts/repositories.  Please feel free to shoot me an email if you have any questions!</p>
+                       <p className="float-right">- Everett</p>
+                    </div>
+                </div>
             </div>
         </div>
-
     )
 }
 export default About;

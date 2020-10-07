aaf3e649baca5b7baf455196a33a564b893bfacc
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-07 19:49:10 -0400

    card shift happens

--- porfolio_react/src/components/ProjectDesc/PasswordGenerator.js
+++ porfolio_react/src/components/ProjectDesc/PasswordGenerator.js
@@ -4,27 +4,44 @@ import openImg from '../Images/screenshots/scrn_passwordGenerator.jpg';
 class PasswordGenerator extends Component {
     render() {
         return (
-            
-                <div className="col m-4">
-                    <div className="card shadow" style={{ width: "375px", height: "100%" }}>
-                        <h5 className="card-header">Password Generator</h5>
-                        <div className="card-body">
-                            <div className="row imageCont">
-                                <div className="col">
-                                    <img className="picture" alt="passwordgenerator" src={openImg} />
-                                </div>
+
+            <div className="col m-4">
+                <div className="card shadow" style={{ width: "375px", height: "100%" }}>
+                    <h5 className="card-header">Password Generator</h5>
+                    <div className="card-body">
+                    <div className="row coverDiv">
+                            <div className="col">
+
+
+
                             </div>
-                            <div className="row">
-                                <div className="col">
-                                    <img src="https://img.shields.io/badge/HTML5-informational?style=flat&logo=html5&logoColor=white&color=b7b7a4" alt="HTML" />
-                                    <img src="https://img.shields.io/badge/CSS3-informational?style=flat&logo=css3&logoColor=white&color=b7b7a4" alt="css" />
-                                    <img src="https://img.shields.io/badge/JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=b7b7a4" alt="javascript" />
-                                </div>
+                        </div>
+                        <div className="row imageCont">
+                            <div className="col">
+                                <img className="imgHov" alt="passwordgenerator" src={openImg} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <img src="https://img.shields.io/badge/HTML5-informational?style=flat&logo=html5&logoColor=white&color=b7b7a4" alt="HTML" />
+                                <img src="https://img.shields.io/badge/CSS3-informational?style=flat&logo=css3&logoColor=white&color=b7b7a4" alt="css" />
+                                <img src="https://img.shields.io/badge/JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=b7b7a4" alt="javascript" />
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col">
+                                <a rel="noopener noreferrer" target="_blank" href="https://github.com/diuguide/password_generator">
+                                    <img id="github" src="https://img.shields.io/badge/REPO-Github-informational?style=flat&logo=github&logoColor=white" alt="github" />
+                                </a>
+                                <a rel="noopener noreferrer" target="_blank" href="https://diuguide.github.io/password_generator/">
+                                    <img id="deployment" src="https://img.shields.io/badge/DEPLOYMENT-informational?style=flat" alt="github" />
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-            
+            </div>
+
         )
     }
 }
--- porfolio_react/src/components/ProjectDesc/WeatherMap.js
+++ porfolio_react/src/components/ProjectDesc/WeatherMap.js
@@ -4,29 +4,46 @@ import openImg from '../Images/screenshots/scrn_weathermap.png';
 class WeatherMap extends Component {
     render() {
         return (
-           
-                <div className="col m-4">
-                    <div className="card shadow" style={{ width: "375px", height: "100%" }}>
-                        <h5 className="card-header">Daily Weather Map</h5>
-                        <div className="card-body">
-                            <div className="row imageCont">
-                                <div className="col">
-                                    <img className="picture" alt="isitopen" src={openImg} />
-                                </div>
+
+            <div className="col m-4">
+                <div className="card shadow" style={{ width: "375px", height: "100%" }}>
+                    <h5 className="card-header">Daily Weather Map</h5>
+                    <div className="card-body">
+                    <div className="row coverDiv">
+                            <div className="col">
+
+
+
                             </div>
-                            <div className="row">
-                                <div className="col">
-                                    <img src="https://img.shields.io/badge/HTML5-informational?style=flat&logo=html5&logoColor=white&color=b7b7a4" alt="HTML" />
-                                    <img src="https://img.shields.io/badge/CSS3-informational?style=flat&logo=css3&logoColor=white&color=b7b7a4" alt="css" />
-                                    <img src="https://img.shields.io/badge/JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=b7b7a4" alt="javascript" />
-                                    <img src="https://img.shields.io/badge/jQuery-informational?style=flat&logo=jquery&logoColor=white&color=b7b7a4" alt="jQuery" />
-                                    <img src="https://img.shields.io/badge/Bootstrap-informational?style=flat&logo=bootstrap&logoColor=white&color=b7b7a4" alt="bootstrap" />
-                                </div>
+                        </div>
+                        <div className="row imageCont">
+                            <div className="col">
+                                <img className="imgHov" alt="isitopen" src={openImg} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <img src="https://img.shields.io/badge/HTML5-informational?style=flat&logo=html5&logoColor=white&color=b7b7a4" alt="HTML" />
+                                <img src="https://img.shields.io/badge/CSS3-informational?style=flat&logo=css3&logoColor=white&color=b7b7a4" alt="css" />
+                                <img src="https://img.shields.io/badge/JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=b7b7a4" alt="javascript" />
+                                <img src="https://img.shields.io/badge/jQuery-informational?style=flat&logo=jquery&logoColor=white&color=b7b7a4" alt="jQuery" />
+                                <img src="https://img.shields.io/badge/Bootstrap-informational?style=flat&logo=bootstrap&logoColor=white&color=b7b7a4" alt="bootstrap" />
+                            </div>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col">
+                                <a rel="noopener noreferrer" target="_blank" href="https://github.com/diuguide/weather_map">
+                                    <img id="github" src="https://img.shields.io/badge/REPO-Github-informational?style=flat&logo=github&logoColor=white" alt="github" />
+                                </a>
+                                <a rel="noopener noreferrer" target="_blank" href="https://diuguide.github.io/weather_map/">
+                                    <img id="deployment" src="https://img.shields.io/badge/DEPLOYMENT-informational?style=flat" alt="github" />
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
-           
+            </div>
+
         )
     }
 }

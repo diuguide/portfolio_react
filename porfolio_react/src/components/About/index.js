2740fa09e5cc287497878cee45c2ffb3c86c1d9d
diuguide <61917141+diuguide@users.noreply.github.com>
2020-10-27 23:48:30 -0400

    added a bio

--- porfolio_react/src/components/About/index.js
+++ porfolio_react/src/components/About/index.js
@@ -6,7 +6,7 @@ function About(props) {
             <div className="col">
                 <div className="row">
                     <div className="col m-1 shadow bg-light rounded">
-                        <div className="pt-2 pl-2">Hello World!</div>
+                        <div className="pt-2 pl-2">Web developer with an eye for design and a proven ability to focus on the customer experience while enhancing a brand message.  I excel in fast paced, collaborative team environments that require  attention to detail, hard work, and the ability to adapt. My objective is to join an entrepreneurial web development team with a focus on mobile-first design and building a better web-experience.</div>
                         <div className="p-2">Thank you for taking the time to visit my portfolio, here you will find links to all of my work in addition to links to my social media and professional accounts/repositories.  Please feel free to shoot me an email if you have any questions!</div>
                         <div className="float-right pr-4 pb-2">- Everett</div>
                     </div>
